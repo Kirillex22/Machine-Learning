6d9ba538d982fe908b2e5bc0990217f0f70767e3
Кирилл <ziabrew34@gmail.com>
2023-11-07 00:44:07 +0600

    lab3_final

--- models/LinearRegression.py
+++ models/LinearRegression.py
@@ -1,15 +1,41 @@
 import numpy as np
-
+import Metrics
 
 class MyLinearRegression:
-
+       
+    def __grad(self, k, X):
+        
+        coef = 0 
+        for i in range(X.shape[0]):
+            coef += X[i, k]
+            
+        return coef
+    
+    def __func(self, X):
+        
+        return (X*self.w).sum(axis = 1)
+        
     def __init__(self, num_iter, learning_rate):
-        self.w = np.array([])
+    
+        self.w = np.array()
         self.num_iter = num_iter
         self.learning_rate = learning_rate
 
     def fit(self, X, y):
+        
+        grad_coefs = []
+        
+        for i in range(X.shape[1]):
+            grad_coefs.append(__grad(i, X))  
+
+        grad = np.array(grad_coefs)
+        
+        self.w = ones(X.shape[1])         
+        
         for i in range(0, num_iter):
-            L_grad = []
-            se
+            
+            self.w = self.w - self.learning_rate*L_grad
+
+    def predict(self, X):
+        return __func(X)
         
--- models/Metrics.py
+++ models/Metrics.py
@@ -1,72 +1,38 @@
 import numpy as np
-from math import sqrt
    
 def mean_abs_err(real: np.ndarray, predicted: np.ndarray) -> float:
 
-   m = predicted.shape[0]
-   sum = 0
+    result = np.mean(abs(real - predicted))
 
-   for i in range(0,m):
-       y_pred = predicted[i]
-       y_real = real[i]
-       sum += abs(y_real - y_pred)
-
-   result = sum/m
-
-   return result
+    return result
 
 
 def mean_squared_err(real: np.ndarray, predicted: np.ndarray) -> float:
-    
-    m = predicted.shape[0]
-    sum = 0
-    
-    for i in range(0,m):
-        y_pred = predicted[i]
-        y_real = real[i]
-        sum += (y_real - y_pred)**2
-    
-    result = sum/m
+
+    result = np.mean((real - predicted)**2)
     
     return result
     
 
 def root_mse(real: np.ndarray, predicted: np.ndarray) -> float:
     
-    result = sqrt(mean_squared_err(predicted, real))
+    result = np.sqrt(mean_squared_err(predicted, real))
         
     return result
 
 
 def mean_abs_perc_err(real: np.ndarray, predicted: np.ndarray) -> float:
-    
-    m = predicted.shape[0]
-    sum = 0
-    
-    for i in range(0,m):
-        y_pred = predicted[i]
-        y_real = real[i]
-        if y_real == 0:
-            continue
-        sum += abs(1 - y_pred/y_real)
-    
-    result = sum/m
+
+    result = np.mean(abs((real - predicted)/real))
     
     return result
     
 
 def r_squared(real: np.ndarray, predicted: np.ndarray) -> float:
-    
-    m = predicted.shape[0]
-    sum = 0
-    y_mean = real.mean()
+
+    y_mean = real.mean()    
     mse = mean_squared_err(real, predicted)
-    
-    for i in range(0,m):
-        y_real = real[i]
-        sum += (y_real - y_mean)**2
-    
-    sum /= m
+    sum = np.mean((real - y_mean)**2)
 
     result = 1 - mse/sum
     
