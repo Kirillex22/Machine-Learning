c8bbbebbb8e3b340a62d925a4f3124feb7f76239
Кирилл <ziabrew34@gmail.com>
2023-12-13 01:24:11 +0600

    lab6 + kmeans final

---
+++ models/kmeans.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class Kmeans:
+       
+    def euclid(self, x_1, x_2):
+        return np.sqrt(np.sum((x_2 - x_1)**2))
+
+    def __init__(self, k = 1, max_iter = 10000, eps = 1e-6):
+        self.k = k
+        self.max_iter = max_iter
+        self.eps = eps
+
+    def fit(self, X):
+        self.predict = np.empty(X.shape[0])
+        self.labels = np.arange(0, self.k, 1)
+        self.centers = X[np.random.choice(X.shape[0], self.k, replace=False)]
+
+        for j in range(self.max_iter):
+            last_predict = self.predict
+            self.update_predicts(X)
+            
+            if ((last_predict == self.predict).any()):
+                break
+                
+            last_centers = self.centers
+            self.update_centers(X)
+
+            if (np.mean((last_centers - last_center)**2) < eps):
+                break        
+
+
+    def update_predicts(self, X):
+        for i in range(X.shape[0]):
+            clusters = [self.euclid(X[i], centr) for centr in self.centers]
+            cluster = clusters.index(min(clusters))
+            self.predict[i] = cluster
+
+    def update_centers(self, X):
+        for label in self.labels:
+            indices = [i for i, x in enumerate(self.predict) if x==label]
+            self.centers[label] = np.mean(X[indices])
+            
+            
+
+
